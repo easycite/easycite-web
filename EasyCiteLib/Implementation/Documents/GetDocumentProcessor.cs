07e13f549e30dc5eb380b3cc94775a02ba56d490
Garrett Oelze <garrett@oelze.com>
2020-03-30 20:16:48 -0500

    Implemented GetDocumentProcessor

---
+++ EasyCiteLib/Implementation/Documents/GetDocumentProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyCiteLib.Interface.Documents;
+using EasyCiteLib.Repository;
+
+namespace EasyCiteLib.Implementation.Documents
+{
+    public class GetDocumentProcessor : IGetDocumentProcessor
+    {
+        private readonly DocumentContext _context;
+
+        public GetDocumentProcessor(DocumentContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IList<Document>> GetDocumentsAsync(IEnumerable<string> documentIds)
+        {
+            return _context.GetDocumentsAsync(documentIds);
+        }
+    }
+}
